5fe3f17b6d32161ed6812f8d30cddb30fef96400
Nevin <n.boyd1@yahoo.com>
2025-09-06 00:43:52 -0700

    Deleted Comment out to run module.exports

--- index.js
+++ index.js
@@ -1,8 +1,40 @@
 //Write your code here
+let attendee = {
+  attendeeId: "T001",
+  name: "Alice Smith",
+  event: "JavaScript Conference",
+  ticketType: "VIP",
+  ticketPrice: 150.00
+};
 
+function logAttendeeName(attendee) {
+  console.log(attendee.name);
+}
 
+function logTicketPrice(attendee) {
+  console.log(attendee.ticketPrice);
+}
 
-//Needed for the tests to work. Don't modify
+function updateTicketType(attendee, newTicketType) {
+  attendee.ticketType = newTicketType;
+  console.log(`${attendee.ticketType}`);
+}
+
+function updateTicketPrice(attendee, newTicketPrice) {
+  attendee.ticketPrice = newTicketPrice;
+  console.log(`${attendee.ticketPrice}`);
+}
+
+function removeEventProperty(attendee) {
+  delete attendee.event;
+  console.log("Event property removed.");
+}
+
+function addCheckedInProperty(attendee) {
+  attendee.checkedIn = true;
+  console.log("Checked-in property added.");
+}
+
 module.exports = {
   ...(typeof attendee !== 'undefined' && { attendee }),
   ...(typeof logAttendeeName !== 'undefined' && { logAttendeeName }),
